79f19ab319ae40a9d9a6fc7f5fb96929b69425df
pyuan-21 <yuanputianatin@outlook.com>
2022-09-26 13:58:33 +0200

    upload the basic things

---
+++ Assets/02 - Scripts/02 - Instance Brushes/RemoveInstanceBrush.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RemoveInstanceBrush : InstanceBrush {
+
+    public override void draw(float x, float z) {
+        terrain.RemoveTreeInstance(x, z, radius);
+    }
+}
