eb27bec2e40d020237debcce855da5ea435c0836
pyuan-21 <yuanputianatin@outlook.com>
2022-10-03 14:39:51 +0200

    SmoothBrush

---
+++ Assets/02 - Scripts/01 - Terrain Brushes/SmoothBrush.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class SmoothBrush : TerrainBrush {
+    public int kernelType = 0; // TODO: create two gaussian kernel to do the convolution
+    public override void draw(int x, int z) {
+
+        for (int zi = -radius; zi <= radius; zi++) {
+            for (int xi = -radius; xi <= radius; xi++) {
+                //float height = // TODO: get this grid height by using the convolution result
+                //terrain.set(x + xi, z + zi, height);
+                
+            }
+        }
+    }
+}
