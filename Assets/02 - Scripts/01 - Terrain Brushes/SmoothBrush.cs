52378ebac687ec30e55db9cd19d4b78256cbe3c9
pyuan-21 <yuanputianatin@outlook.com>
2022-10-03 15:38:08 +0200

    Update SmoothBrush.cs

--- Assets/02 - Scripts/01 - Terrain Brushes/SmoothBrush.cs
+++ Assets/02 - Scripts/01 - Terrain Brushes/SmoothBrush.cs
@@ -4,14 +4,33 @@ using UnityEngine;
 
 
 public class SmoothBrush : TerrainBrush {
-    public int kernelType = 0; // TODO: create two gaussian kernel to do the convolution
+    private float[] gaussianKernel = {
+        1.0f/16, 2.0f/16, 1.0f/16,
+        2.0f/16, 4.0f/16, 2.0f/16,
+        1.0f/16, 2.0f/16, 1.0f/16,
+    };
     public override void draw(int x, int z) {
-
+        int minX = x - radius;
+        int maxX = x + radius;
+        int minZ = z - radius;
+        int maxZ = z + radius;
         for (int zi = -radius; zi <= radius; zi++) {
             for (int xi = -radius; xi <= radius; xi++) {
-                //float height = // TODO: get this grid height by using the convolution result
-                //terrain.set(x + xi, z + zi, height);
-                
+                // convolution
+                int centerX = x + xi;
+                int centerZ = z + zi;
+                float height = 0.0f;
+                for(int i = -1; i <= 1; i++)
+                {
+                    for(int j = -1; j <= 1; j++)
+                    {
+                        int curX = Mathf.Clamp(centerX+i, minX, maxX);
+                        int curY = Mathf.Clamp(centerZ+j, minZ, maxZ);
+                        height += gaussianKernel[i + 1 + (j + 1) * 3] * terrain.get(curX, curY);
+                    }
+                }
+                terrain.set(x + xi, z + zi, height);
+
             }
         }
     }
