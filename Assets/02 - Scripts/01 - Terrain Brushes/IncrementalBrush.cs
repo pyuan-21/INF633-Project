6a2cef2f339a9fc844b511a8bce3cad011d13bd3
pyuan-21 <yuanputianatin@outlook.com>
2022-10-03 14:19:22 +0200

    Brush

--- Assets/02 - Scripts/01 - Terrain Brushes/IncrementalBrush.cs
+++ Assets/02 - Scripts/01 - Terrain Brushes/IncrementalBrush.cs
@@ -2,21 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GaussianBrush : TerrainBrush {
-
-    public float maxHeight = 5;
-    public float varianceX = 10;
-    public float varianceY = 10;
-    private float Gaussian2D(float x, float y)
-    {
-        return maxHeight * Mathf.Exp(-x * x * 0.5f / (varianceX * varianceX) - y * y * 0.5f / (varianceY * varianceY));
-    }
-
+public class IncrementalBrush : TerrainBrush {
+    public bool isIncreasing = true;
+    public float incrementalHeight = 5;
     public override void draw(int x, int z) {
         for (int zi = -radius; zi <= radius; zi++) {
             for (int xi = -radius; xi <= radius; xi++) {
                 float height = terrain.get(x + xi, z + zi);
-                height += Gaussian2D(xi, zi);
+                height += incrementalHeight;
                 terrain.set(x + xi, z + zi, height);
             }
         }
