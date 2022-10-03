43c0dd6fcae44f2b2b44563e1c4cdac3c6a3f93e
pyuan-21 <yuanputianatin@outlook.com>
2022-10-03 15:45:36 +0200

    randombrush

---
+++ Assets/02 - Scripts/01 - Terrain Brushes/RandomBrush.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomBrush : TerrainBrush {
+    public float minHeight = 2;
+    public float maxHeight = 5;
+    public override void draw(int x, int z) {
+        for (int zi = -radius; zi <= radius; zi++) {
+            for (int xi = -radius; xi <= radius; xi++) {
+                float height = Random.Range(minHeight, maxHeight);
+                height += terrain.get(x + xi, z + zi);
+                terrain.set(x + xi, z + zi, height);
+            }
+        }
+    }
+}
