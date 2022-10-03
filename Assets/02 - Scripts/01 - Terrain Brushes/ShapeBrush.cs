89665cc9a84069c2d0b0c503117548e868a9d296
pyuan-21 <yuanputianatin@outlook.com>
2022-10-03 14:32:36 +0200

    update brush

---
+++ Assets/02 - Scripts/01 - Terrain Brushes/ShapeBrush.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum BrushShape
+{
+    SQUARE,
+    CIRCLE,
+}
+public class ShapeBrush : TerrainBrush {
+    public bool fixedHeight = true;
+    public bool isIncreasing = true;
+    public float incrementalHeight = 5;
+    public BrushShape shapeType = BrushShape.SQUARE;
+    public override void draw(int x, int z) {
+        float radiusSquare = radius * radius;
+
+        for (int zi = -radius; zi <= radius; zi++) {
+            for (int xi = -radius; xi <= radius; xi++) {
+                if(shapeType == BrushShape.CIRCLE)
+                {
+                    if ((xi * xi + zi * zi) > radiusSquare)
+                        continue; // not inside the circle
+                }
+                if (fixedHeight)
+                {
+                    terrain.set(x + xi, z + zi, incrementalHeight);
+                }
+                else
+                {
+                    float height = terrain.get(x + xi, z + zi);
+                    if (isIncreasing)
+                        height += incrementalHeight;
+                    else
+                        height -= incrementalHeight;
+                    terrain.set(x + xi, z + zi, height);
+                }
+            }
+        }
+    }
+}
