10e6d6bcaa39b5f6b4a5bafc38e6a2827824c9f1
SALMA CHANA <chsalma22@gmail.com>
2022-10-14 16:53:40 +0200

    minimal distance brush

--- Assets/02 - Scripts/02 - Instance Brushes/MinimalDistanceBrush.cs
+++ Assets/02 - Scripts/02 - Instance Brushes/MinimalDistanceBrush.cs
@@ -4,11 +4,30 @@ using UnityEngine;
 
 public class MinimalDistanceBrush : InstanceBrush
 {
+    public float mindistance = 15f;
 
+
+    public bool checkMinDistance(float x,float z)
+    {
+        float distance = float.MaxValue;
+        var n = terrain.getObjectCount();
+        for (int i = 0; i < n; i++)
+        {
+            Vector3 objLoc = terrain.getObjectLoc(i);
+            // distance to nearest instances
+            distance = Mathf.Min(distance, Mathf.Sqrt((objLoc.x - x) * (objLoc.x - x) + (objLoc.z - z) * (objLoc.z - z)));
+        }
+
+        print(distance);
+
+        if (distance < mindistance) return false;
+        else return true;
+    }
     public override void draw(float x, float z)
     {
-       
-      
+
+        if (checkMinDistance(x, z)) spawnObject(x, z);
+        
     }
 }
 
