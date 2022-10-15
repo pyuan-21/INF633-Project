07929c167941592d6ca6c28ff1cd69783a2a33b3
SALMA CHANA <chsalma22@gmail.com>
2022-10-15 14:45:15 +0200

    Update ShapeInstanceBrush.cs

--- Assets/02 - Scripts/02 - Instance Brushes/ShapeInstanceBrush.cs
+++ Assets/02 - Scripts/02 - Instance Brushes/ShapeInstanceBrush.cs
@@ -4,43 +4,69 @@ using UnityEngine;
 
 public class ShapeInstanceBrush : InstanceBrush
 {
-    public float mindistance = 15f;
+    public enum ListOfShape { BASIC, CIRCLE, SQUARE, GRID };
+    public ListOfShape shapeType;
+    public float max_height = 20f;
+    public  float max_angle = 1f;
+     
+
     public GameObject[] treesobjects = new GameObject[3];
 
-    public bool checkMinDistance(float x,float z)
-    {
-        float distance = float.MaxValue;
-        var n = terrain.getObjectCount();
-        for (int i = 0; i < n; i++)
-        {
-            Vector3 objLoc = terrain.getObjectLoc(i);
-            // distance to nearest instances
-            distance = Mathf.Min(distance, Mathf.Sqrt((objLoc.x - x) * (objLoc.x - x) + (objLoc.z - z) * (objLoc.z - z)));
-        }
 
-        //print(distance);
-
-        if (distance < mindistance) return false;
-        else return true;
-    }
 
   
     public override void draw(float x, float z)
     {
-    
 
-        // altitude
-        float height = terrain.get(x, z);
-        if (height <= 20f && height > 10f) terrain.object_prefab = treesobjects[2];
-        if (height <= 10f && height > 5f) terrain.object_prefab = treesobjects[1];
-        if (height <= 5f) terrain.object_prefab = treesobjects[0];
-        print(height + "|" + terrain.object_prefab);
-        // steepness
-        if (1f < terrain.getSteepness(x, z)) return;
-
-
-        if (checkMinDistance(x, z)) spawnObject(x, z);
         
+
+          // altitude
+        float height = terrain.get(x, z);
+        if (height <= max_height/4) terrain.object_prefab = treesobjects[0];
+        if (height <= max_height/2 && height > max_height/4) terrain.object_prefab = treesobjects[1];
+        if (height <= max_height && height > max_height/2) terrain.object_prefab = treesobjects[2];
+
+
+        print(height+"|"+terrain.object_prefab);
+
+        // steepness
+        if (terrain.getSteepness(x, z) >max_angle) return;
+
+        
+        if (shapeType == ListOfShape.BASIC) spawnObject(x, z);
+
+
+        if (shapeType == ListOfShape.CIRCLE)
+        {
+            float r = Random.Range(0f, radius);
+            float angle = Random.Range(0f, 2 * Mathf.PI);
+            spawnObject(x + r * Mathf.Cos(angle), z + r * Mathf.Sin(angle));
+        }
+
+        if (shapeType == ListOfShape.SQUARE)
+        {
+            float x1 = Random.Range(-radius, radius);
+            float z1 = Random.Range(-radius, radius);
+            spawnObject(x + x1, z + z1);
+        }
+
+        if (shapeType == ListOfShape.GRID)
+        {
+            for (float dz = -radius;  dz<= radius; dz++)
+            {
+                for (float dx = -radius; dx <= radius;  dx++)
+                {
+                        spawnObject(x + dx, z + dx);
+
+                }
+            }
+        }
+
+
+
+
+
+
     }
 }
 
