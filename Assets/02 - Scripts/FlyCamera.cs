c482277ef02357877c69960cccf1f9ba8b047996
pyuan-21 <yuanputianatin@outlook.com>
2022-10-16 19:34:53 +0200

    update

---
+++ Assets/02 - Scripts/FlyCamera.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyCamera : MonoBehaviour {
+
+    public float moveSpeed = 1.0f;
+    public float rotateSpeed = 1.0f;
+    void Start () {
+        
+    }
+
+    void Update () {
+        // moving
+        Vector2 dir = Vector2.zero;
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            dir += Vector2.up;
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            dir += Vector2.down;
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            dir += Vector2.left;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            dir += Vector2.right;
+        }
+        Vector2 offset = dir * moveSpeed;
+        transform.position = new Vector3(transform.position.x + offset.x, transform.position.y, transform.position.z + offset.y);
+
+        // rotating
+        float angleAroundX = 0;
+        float angleAroundY = 0;
+        float angleAroundZ = 0;
+        if (Input.GetKey(KeyCode.A))
+        {
+            angleAroundY += rotateSpeed;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            angleAroundY -= rotateSpeed;
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            angleAroundX -= rotateSpeed;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            angleAroundX += rotateSpeed;
+        }
+        if (Input.GetKey(KeyCode.Q))
+        {
+            angleAroundZ += rotateSpeed;
+        }
+        if (Input.GetKey(KeyCode.E))
+        {
+            angleAroundZ -= rotateSpeed;
+        }
+
+        if (Mathf.Abs(angleAroundX) >= 1)
+        {
+            transform.rotation *= Quaternion.AngleAxis(angleAroundX, Vector3.right);
+        }
+
+        if (Mathf.Abs(angleAroundY) >= 1)
+        {
+            transform.rotation *= Quaternion.AngleAxis(angleAroundY, Vector3.up);
+        }
+
+        if (Mathf.Abs(angleAroundZ) >= 1)
+        {
+            transform.rotation *= Quaternion.AngleAxis(angleAroundZ, Vector3.back);
+        }
+
+        if (Input.GetKey(KeyCode.Z))
+        {
+            // go higher
+            transform.position += Vector3.up * moveSpeed;
+        }
+
+        if (Input.GetKey(KeyCode.X))
+        {
+            // go down
+            transform.position += Vector3.down * moveSpeed;
+        }
+    }
+}
